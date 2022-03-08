b4cb839ae97ad42716f765c0480c125aae870c61
五茶 <2490445193@qq.com>
2022-03-08 20:59:05 +0800

    ✨ 功能: 抹平各端safe-area-inset-*兼容

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,28 @@
 </script>
 <style lang="scss">
   @import 'uview-ui/index.scss';
+  page {
+    --safe-area-inset-top: 0px;
+    --safe-area-inset-right: 0px;
+    --safe-area-inset-bottom: 0px;
+    --safe-area-inset-left: 0px;
+  }
+
+  @supports (top: constant(safe-area-inset-top)) {
+    page {
+      --safe-area-inset-top: constant(safe-area-inset-top);
+      --safe-area-inset-right: constant(safe-area-inset-right);
+      --safe-area-inset-bottom: constant(safe-area-inset-bottom);
+      --safe-area-inset-left: constant(safe-area-inset-left);
+    }
+  }
+
+  @supports (top: env(safe-area-inset-top)) {
+    page {
+      --safe-area-inset-top: env(safe-area-inset-top);
+      --safe-area-inset-right: env(safe-area-inset-right);
+      --safe-area-inset-bottom: env(safe-area-inset-bottom);
+      --safe-area-inset-left: env(safe-area-inset-left);
+    }
+  }
 </style>
