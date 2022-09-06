fd6d8650c941553efcdb31fc39452b8e7b237828
PentaTea <2490445193@qq.com>
2022-09-07 00:38:02 +0800

    🎨 重构: unocss小程序适配策略

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
+import type { Preset, SourceCodeTransformer } from 'unocss'
+import {
+  defineConfig,
+  presetUno,
+  presetAttributify,
+  presetIcons,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+import {
+  presetApplet,
+  presetRemToRpx,
+  transformerApplet,
+  transformerAttributify,
+} from 'unocss-applet'
 import { times } from 'lodash'
 import path from 'path'
+
+const presets: Preset[] = []
+const transformers: SourceCodeTransformer[] = []
+const isMP = !!process.env.UNI_PLATFORM?.startsWith('mp')
+
+if (!isMP) {
+  presets.push(presetAttributify())
+} else {
+  transformers.push(transformerAttributify())
+  transformers.push(transformerApplet())
+}
 
 export default defineConfig({
   theme: {
@@ -8,24 +33,16 @@ export default defineConfig({
       ...['none', 'xs', 'sm', 'DEFAULT', 'lg', 'xl'],
       ...times(8).map(n => n + 2 + 'xl'),
     ].reduce((o, k, i) => {
-      if (process.env.UNI_PLATFORM?.startsWith('mp')) o[k] = (i * 10).toFixed(2) + 'rpx'
+      if (isMP) o[k] = (i * 10).toFixed(2) + 'rpx'
       else o[k] = (i / 3.2).toFixed(2) + 'rem'
       return o
     }, {}),
   },
-  presets: [presetUno(), presetAttributify(), presetIcons()],
+  presets: [presetUno(), presetIcons(), ...presets],
+  transformers: [transformerDirectives(), transformerVariantGroup(), ...transformers],
   shortcuts: {
     'flex-center': 'flex flex-row justify-center items-center',
     'flex-center-col': 'flex flex-col justify-center items-center',
   },
-  postprocess: [
-    // 小程序不需要属性选择器
-    process.env.UNI_PLATFORM?.startsWith('mp') &&
-      (util => {
-        if (!util.selector.startsWith('[')) return
-        util.selector = undefined
-        util.entries = []
-      }),
-  ].filter(e => !!e),
   include: [path.resolve(__dirname, 'src', '**')],
 })
